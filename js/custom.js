3c4b757038a84a1b8420e1ee2de94a7d423a18b6
woojoo414 <woojoo414@naver.com>
2021-07-08 11:52:33 +0900

    2depth focus 이벤트 연동

--- js/custom.js
+++ js/custom.js
@@ -15,3 +15,14 @@ $("#gnb>li").on("mouseenter", function(){
 $("#gnb>li").on("mouseleave", function(){
     $(this).find(".sub").hide();
 });
+
+$("#gnb>li").each(function(index){
+    //1depth 1에서 첫번쨰 a요소에 focusin 이벤트 연결
+    $("#gnb>li").eq(index).find("a").first().on("focusin", function(){
+        $("#gnb>li").eq(index).find(".sub").show();
+    })
+    //1depth li에서 마지막 a요소에 focusout 이벤트 연결
+    $("#gnb>li").eq(index).find("a").last().on("focusout", function(){
+        $("#gnb>li").eq(index).find(".sub").hide();
+    })
+})
