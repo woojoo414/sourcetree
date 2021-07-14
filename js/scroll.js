1b07c5c58ab546b05785ce755889667f6addc796
woojoo414 <woojoo414@naver.com>
2021-07-14 11:36:47 +0900

    세로 스크롤 조정

--- js/scroll.js
+++ js/scroll.js
@@ -3,11 +3,15 @@ var posArr = [];
 //세로 위치값을 구할 요소들을 공통클래스명으로 선택
 var scrolls = $(".myScroll");
 
+var base = -200;
+
+
 function setPos(){
     //브라우저 리사이즈시 기존 값을 비우고 
     //새로운 위치값을 다시 저장하기 위해 배열 초기화
     posArr = [];
     $(scrolls).each(function(index, data){
+        console.log(data);
         posArr.push($(data).offset().top);
     });
     
@@ -26,30 +30,42 @@ $(window).on("scroll",function(){
 
     //첫번째 박스 위치값보다 스크롤이 더 많이 되면
     //첫번째 버튼 활성화
-    if(scroll >= posArr[0]){
+    if(scroll >= posArr[0] + base){
         $(".vNavi li a").removeClass("on");
         $(".vNavi li").eq(0).children("a").addClass("on");
+
+        $(".myScroll").removeClass("on");
+        $(".myScroll").eq(0).addClass("on");
     }
 
     //두번째 박스 위치값보다 스크롤이 더 많이 되면
     //두번째 버튼 활성화
-    if(scroll >= posArr[1]){
+    if(scroll >= posArr[1] + base){
         $(".vNavi li a").removeClass("on");
         $(".vNavi li").eq(1).children("a").addClass("on");
+
+        $(".myScroll").removeClass("on");
+        $(".myScroll").eq(1).addClass("on");
     }
 
     //세번째 박스 위치값보다 스크롤이 더 많이 되면
     //세번째 버튼 활성화
-    if(scroll >= posArr[2]){
+    if(scroll >= posArr[2] + base){
         $(".vNavi li a").removeClass("on");
         $(".vNavi li").eq(2).children("a").addClass("on");
+
+        $(".myScroll").removeClass("on");
+        $(".myScroll").eq(2).addClass("on");
     }
 
     //네번째 박스 위치값보다 스크롤이 더 많이 되면
     //네번째 버튼 활성화
-    if(scroll >= posArr[3]){
+    if(scroll >= posArr[3] + base){
         $(".vNavi li a").removeClass("on");
         $(".vNavi li").eq(3).children("a").addClass("on");
+
+        $(".myScroll").removeClass("on");
+        $(".myScroll").eq(3).children("a").addClass("on");
     }
 });
 
